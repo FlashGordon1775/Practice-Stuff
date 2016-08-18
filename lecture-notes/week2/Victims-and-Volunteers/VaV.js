bfb0f9dad8a7842396724e14178dff66d35f9912
flashgordon1775 <flashg1775@gmail.com>
2016-08-18 14:50:39 -0600

    Victims and volunteers. No bonus done yet.

--- lecture-notes/week2/Victims-and-Volunteers/VaV.js
+++ lecture-notes/week2/Victims-and-Volunteers/VaV.js
@@ -1,18 +1,32 @@
 //My arrays are not working yet
 
 
+
+
+var victimArray = [];
+var phoneArray = [];
+var streetArray = [];
+
+var volArray = [];
+var volPhoneArray = [];
+var volStreetArray = [];
+
+var numberVictimsNumber;
+var numberVolunteersNumber;
+
 victims();
 
-function victims(){
+function victims(numberVictims){
     var numberVictims = prompt("How many victims are you reporting?");
-    var numberVictimsNumber = parseInt(numberVictims);
+    numberVictimsNumber = parseInt(numberVictims);
     if(numberVictimsNumber > 0){
         var i = numberVictimsNumber
         for(var i = 0; i < numberVictimsNumber; i++){
             //ask for things
-            name();
-            phone();
-            street();
+            addName();
+            addPhone();
+            addStreet();
+            
         }
         
     }else{
@@ -21,39 +35,90 @@ function victims(){
     }
 }
 
-var namesArray = [];
-
-function name(){
+function addName(victimName){
     var victimName = prompt("Victim's name:");
-    //var namesArray = [];
-    //var newNamesArray = NamesArray.push(victimName);
-    console.log(victimName);
+    victimArray.push(victimName);
+    console.log(victimName, victimArray);
 }
 
     
 
-function phone(){
+function addPhone(phoneNumber){
     var phoneNumber = prompt("Victim's phone number:");
-    //var phoneArray = [];
     if (phoneNumber.match(/^\d{3}-\d{3}-\d{4}$/)){
         alert ("Valid Number");
-        //var newPhoneArray = phoneArray.push(phoneNumber);
-        console.log(phoneNumber);
+        phoneArray.push(phoneNumber);
+        console.log(phoneNumber, phoneArray);
     }else{
         alert ("Invalid Number");
-        phone();
+        addPhone();
     }
     
 }
 
-//var phoneArray = [];
 
-function street(){
+
+function addStreet(streetName){
     var streetName= prompt("Victim's location:");
-    //var streetArray = [];
-    //var newStreetArray = streetArray.push(streetName);
-    console.log(streetName);
+    streetArray.push(streetName)
+    console.log(streetName, streetArray);
 }
 
-//var streetArray = [];
+volunteers();
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+
+
+function volunteers(){
+    var numberVolunteers = prompt("How many volunteers are you reporting?");
+    numberVolunteersNumber = parseInt(numberVolunteers);
+    if(numberVolunteersNumber > 0){
+        var i = numberVolunteersNumber
+        for(var i = 0; i < numberVolunteersNumber; i++){
+            //ask for things
+            addVolName();
+            addVolPhone();
+            addVolStreet();
+        }
+        
+    }else{
+        alert("Enter valid number.");
+        volunteers();
+    }
+}
+
+function addVolName(volName){
+    var volName = prompt("Volunteer's name:");
+    volArray.push(volName);
+    console.log(volName, volArray);
+}
+
+    
+
+function addVolPhone(volPhonehoneNumber){
+    var volPhoneNumber = prompt("Volunteer's phone number:");
+    if (volPhoneNumber.match(/^\d{3}-\d{3}-\d{4}$/)){
+        alert ("Valid Number");
+        volPhoneArray.push(volPhoneNumber);
+        console.log(volPhoneNumber, volPhoneArray);
+    }else{
+        alert ("Invalid Number");
+        andVolPhone();
+    }
+    
+}
+
+
+
+function addVolStreet(volStreetName){
+    var volStreetName= prompt("Volunteer's location:");
+    volStreetArray.push(volStreetName)
+    console.log(volStreetName, volStreetArray);
+}
+
+alert("There is/are " + numberVictimsNumber + " victim(s)"  + "\n" + "There is/are" + numberVolunteersNumber + " volunteer(s)" +"\n"+
+    "Name of victim(s):" + victimArray +"\n"+ "Name of volunteer(s):" + volArray);
+
+
 
