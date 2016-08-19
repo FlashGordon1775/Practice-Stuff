4d32a7aa20ab654c825226ea6b1dfafb6194d7ba
flashgordon1775 <flashg1775@gmail.com>
2016-08-19 13:52:14 -0600

    Loop practice complete

---
+++ lecture-notes/week2/loop-practice-1/loop-practice.js
@@ -0,0 +1,30 @@
+for (var i = 0; i < 600; i += 100){
+    console.log(i);
+}
+
+for (var i = 1; i <= 65; i = i*2){
+    console.log(i);
+}
+
+for (var i = 1; i <= 3; i++){
+    console.log(i);
+    console.log(i);
+    console.log(i);
+}
+
+for (var i = 0; i < 11; i += 2){
+    console.log(i);
+}
+
+for (var i = 3; i < 16; i += 3){
+    console.log(i);
+}
+
+for (var i = 9; i >= 0; i--){
+    console.log(i);
+}
+
+for (var i = 0; i < 4; i++){
+    for (var j = 0; j < 4; j++){
+       console.log(j)}
+}
